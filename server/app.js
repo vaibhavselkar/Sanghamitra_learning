40f1a638b1b4c706d9be2862b88d40a7283cc593
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-07-04 13:53:41 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -49,7 +49,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24, // 1 day
         httpOnly: true,
-        secure: true,                    // HTTPS only
+        secure: false,                    // HTTPS only
         sameSite: 'none'
     }
 }));
