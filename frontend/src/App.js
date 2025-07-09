222aefdadfbc9c6f24c5007e8539239d0f3cdcc5
vaibhavselkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-07-09 12:17:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-// src/App.js
+// frontend/src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./components/AuthContext";
