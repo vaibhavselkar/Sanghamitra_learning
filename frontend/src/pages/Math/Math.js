3fc225e40695ec302ab9fb704b4f0c072a56725d
Vaibhav Selkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-07-25 13:29:18 +0530

    Update Math.js

--- frontend/src/pages/Math/Math.js
+++ frontend/src/pages/Math/Math.js
@@ -38,27 +38,7 @@ const Math = () => {
       <section id="courses-list" className="section courses-list">
         <div className="container">
           <div className="row">
-            {/* Algebra */}
-            <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
-              <div className="course-item">
-                <img src={algebraImg} className="img-fluid" alt="Algebra" />
-                <div className="course-content">
-                  <div className="d-flex justify-content-between align-items-center mb-3">
-                    <Link to="/math/algebra">
-                      <button className="category">Algebra</button>
-                    </Link>
-                  </div>
-                  <p className="description">
-                    Welcome to our Algebra Arena, where equations are solved and formulas are mastered. Delve into our curated resources and step-by-step tutorials designed to conquer the complexities of algebra. Whether you're simplifying expressions or solving quadratic equations, unleash your mathematical prowess and conquer the world of algebra with confidence.
-                  </p>
-                  <div className="trainer d-flex justify-content-between align-items-center">
-                    <div className="trainer-rank d-flex align-items-center">
-                      <i className="bi bi-heart heart-icon"></i>&nbsp;65
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            
             {/* Arithmetic */}
             <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
               <div className="course-item">
@@ -71,6 +51,27 @@ const Math = () => {
                   </div>
                   <p className="description">
                     Welcome to our arithmetic Learning Hub. Enhance your arithmetic skills with our comprehensive resources and interactive lessons. Join our community and embark on an enriching journey towards mastering arithmetic.
+                  </p>
+                  <div className="trainer d-flex justify-content-between align-items-center">
+                    <div className="trainer-rank d-flex align-items-center">
+                      <i className="bi bi-heart heart-icon"></i>&nbsp;65
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Algebra */}
+            <div className="col-lg-4 col-md-6 d-flex align-items-stretch">
+              <div className="course-item">
+                <img src={algebraImg} className="img-fluid" alt="Algebra" />
+                <div className="course-content">
+                  <div className="d-flex justify-content-between align-items-center mb-3">
+                    <Link to="/math/algebra">
+                      <button className="category">Algebra</button>
+                    </Link>
+                  </div>
+                  <p className="description">
+                    Welcome to our Algebra Arena, where equations are solved and formulas are mastered. Delve into our curated resources and step-by-step tutorials designed to conquer the complexities of algebra. Whether you're simplifying expressions or solving quadratic equations, unleash your mathematical prowess and conquer the world of algebra with confidence.
                   </p>
                   <div className="trainer d-flex justify-content-between align-items-center">
                     <div className="trainer-rank d-flex align-items-center">
