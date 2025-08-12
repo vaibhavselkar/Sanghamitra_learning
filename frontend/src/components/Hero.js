5e19eafe3b4cc898c501c0e75fd8bfcaff98536d
Vaibhav Selkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-08-12 12:41:15 +0530

    Update Hero.js

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -5,11 +5,11 @@ import HeroImage from '../assets/img/Index Page Images.png';
 const Hero = () => {
   return (
     <section id="hero" className="hero section">
-      <img src = {HeroImage} alt="Hero" data-aos="fade-in" />
+      <img src = {HeroImage} alt="Hero" />
       <div className="container">
         <h2 data-aos="fade-up" style={{ animation: "none", caretColor: "transparent" }}>Learning Today, Leading Tomorrow</h2>
         <p data-aos="fade-up" style={{ animation: "none", caretColor: "transparent" }}>Educating Minds, Empowering Futures.</p>
-        <div className="d-flex mt-4" data-aos="fade-up">
+        <div className="d-flex mt-4" >
           <a href="/about" className="btn-get-started" style={{ animation: "none", caretColor: "transparent" }}>Learn More</a>
         </div>
       </div>
