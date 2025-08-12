a9d8d4a8dd1020fd8d63825ff33e38195ec96bba
Vaibhav Selkar <132983753+vaibhavselkar@users.noreply.github.com>
2025-08-12 12:41:47 +0530

    Update Hero.js

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -7,8 +7,8 @@ const Hero = () => {
     <section id="hero" className="hero section">
       <img src = {HeroImage} alt="Hero" />
       <div className="container">
-        <h2 data-aos="fade-up" style={{ animation: "none", caretColor: "transparent" }}>Learning Today, Leading Tomorrow</h2>
-        <p data-aos="fade-up" style={{ animation: "none", caretColor: "transparent" }}>Educating Minds, Empowering Futures.</p>
+        <h2 style={{ animation: "none", caretColor: "transparent" }}>Learning Today, Leading Tomorrow</h2>
+        <p style={{ animation: "none", caretColor: "transparent" }}>Educating Minds, Empowering Futures.</p>
         <div className="d-flex mt-4" >
           <a href="/about" className="btn-get-started" style={{ animation: "none", caretColor: "transparent" }}>Learn More</a>
         </div>
